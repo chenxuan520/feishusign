49798fd945083562967d0cf0db795f7f57faff1d
chenxuan <1607772321@qq.com>
2023-06-14 07:30:05 +0000

    fix:get message bug

--- internel/view/user.go
+++ internel/view/user.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"github.com/larksuite/oapi-sdk-go/v3/service/authen/v1"
+	larkcontact "github.com/larksuite/oapi-sdk-go/v3/service/contact/v3"
 	"gitlab.dian.org.cn/dianinternal/feishusign/internel/logger"
 	"gitlab.dian.org.cn/dianinternal/feishusign/internel/tools"
 	"gitlab.dian.org.cn/dianinternal/feishusign/internel/view/request"
@@ -25,18 +26,46 @@ func UserSignIn(c *gin.Context) {
 		response.Error(c, http.StatusBadRequest, fmt.Errorf("please log first"))
 		return
 	}
-	feishuReq := larkauthen.NewCreateAccessTokenReqBuilder().Body(
+	openIDReq := larkauthen.NewCreateAccessTokenReqBuilder().Body(
 		larkauthen.NewCreateAccessTokenReqBodyBuilder().
 			GrantType("authorization_code").
 			Code(req.Code).
 			Build()).Build()
-	res, err := tools.GlobalLark.Authen.AccessToken.Create(context.Background(), feishuReq, larkcore.WithTenantAccessToken(tools.GetAccessToken()))
+	res, err := tools.GlobalLark.Authen.AccessToken.Create(context.Background(), openIDReq, larkcore.WithTenantAccessToken(tools.GetAccessToken()))
 	if err != nil {
 		logger.GetLogger().Error(fmt.Sprintf("UserSignIn:%s", err.Error()))
 		response.Error(c, http.StatusBadRequest, err)
 		return
 	}
+	if res.Data.UserId == nil {
+		response.ErrorDetail(c, http.StatusBadRequest, map[string]interface{}{"data": res}, fmt.Errorf("get empty userid"))
+		return
+	}
+	userID := *res.Data.UserId
+	fmt.Println(userID)
+	messageReq := larkcontact.NewGetUserReqBuilder().UserId(userID).UserIdType("user_id").Build()
+	resmsg, err := tools.GlobalLark.Contact.User.Get(context.Background(), messageReq, larkcore.WithTenantAccessToken(tools.GetAccessToken()))
+	if err != nil {
+		logger.GetLogger().Error(fmt.Sprintf("UserSignInresMsg:%s", err.Error()))
+		response.Error(c, http.StatusBadRequest, err)
+		return
+	}
+	parts := []string{}
+	for _, v := range resmsg.Data.User.DepartmentIds {
+		partReq := larkcontact.NewGetDepartmentReqBuilder().
+			DepartmentId(v).
+			Build()
+		// 发起请求
+		resPart, err := tools.GlobalLark.Contact.Department.Get(context.Background(), partReq)
+		if err != nil {
+			logger.GetLogger().Error(fmt.Sprintf("GetPart:%s", err.Error()))
+			response.Error(c, http.StatusBadRequest, err)
+			return
+		}
+		parts = append(parts, *resPart.Data.Department.Name)
+	}
 	response.Success(c, map[string]interface{}{
-		"data": res,
+		"data": resmsg,
+		"part": parts,
 	})
 }
